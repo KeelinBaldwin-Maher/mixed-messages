050310aa8e3291b15a1e6a80463ac2b1f756a5d0
Keelin <keelin.baldwinmaher@gmail.com>
2023-12-07 11:37:32 -0700

    Created script to render flower petals falling

---
+++ main.js
@@ -0,0 +1,55 @@
+// Making a 4 point bezier curve
+// I kind of get it, i'm getting the math for this from: https://javascript.info/bezier-curve
+// P = ((1−t)^3 * a) + (3 * (1−t)^2 * t * b) + (3 * (1−t) * t^2 * c) + (t^3 * d)
+function cubicBezier(p1, p2, p3, p4, t) {
+    const point1 = Math.pow((1 - t), 3) * p1;
+    const point2 = 3 * Math.pow((1 - t), 2) * t * p2;
+    const point3 = 3 * (1 - t) * Math.pow(t, 2) * p3;
+    const point4 = Math.pow(t, 3) * p4;
+    return point1 + point2 + point3 + point4;
+}
+
+// 3 point curve
+function quadBezier(p1, p2, p3, t) {
+    const point1 = (Math.pow((1 - t), 2)) * p1;
+    const point2 = 2 * (1 - t) * t * p2;
+    const point3 = (Math.pow(t, 2)) * p3;
+    return point1 + point2 + point3;
+}
+
+function bezierPath(t) {
+    const x1 = 0, x2 = 250, x3 = 20, x4 = 400;
+    const y1 = 0, y2 = 5, y3 = 375, y4 = 400;
+
+    const pointX = cubicBezier(x1, x2, x3, x4, t);
+    const pointY = cubicBezier(y1, y2, y3, y4, t);
+    
+    return {"pointX": pointX, "pointY": pointY};
+}
+
+function flowerMotion(x, y) {
+    const flower = document.querySelector(".flower-test");
+    flower.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+// Based on: https://javascript.info/js-animation#structured-animation
+function animate(duration) {
+    let start = performance.now();
+
+    requestAnimationFrame(function animate(time) {
+        let timeLine = (time - start) / duration; // timeLine goes from 0 to 1
+        if (timeLine > 1) {
+            timeLine = 1;
+        } else if (timeLine < 0) {
+            timeLine = 0;
+        }
+
+        let {pointX, pointY} = bezierPath(cubicBezier(0, 0.35, 0.53, 1, timeLine));
+
+        flowerMotion(pointX, pointY);
+
+        (timeLine < 1) ? requestAnimationFrame(animate) : console.log("Animation done"); // Go to next frame
+    });
+}
+
+animate(3000);
