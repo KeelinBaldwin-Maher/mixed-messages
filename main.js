851ac19a6f46b8fb444cdb315eb439e1369d3aa4
Keelin <keelin.baldwinmaher@gmail.com>
2023-12-07 12:17:37 -0700

    Petal rotation

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-// Making a 4 point bezier curve
-// I kind of get it, i'm getting the math for this from: https://javascript.info/bezier-curve
+// Making a 4 point bezier curve. 
+// The math for this from: https://javascript.info/bezier-curve
 // P = ((1−t)^3 * a) + (3 * (1−t)^2 * t * b) + (3 * (1−t) * t^2 * c) + (t^3 * d)
 function cubicBezier(p1, p2, p3, p4, t) {
     const point1 = Math.pow((1 - t), 3) * p1;
@@ -17,7 +17,7 @@ function quadBezier(p1, p2, p3, t) {
     return point1 + point2 + point3;
 }
 
-function bezierPath(t) {
+function cubicBezierPath(t) {
     const x1 = 0, x2 = 250, x3 = 20, x4 = 400;
     const y1 = 0, y2 = 5, y3 = 375, y4 = 400;
 
@@ -27,26 +27,30 @@ function bezierPath(t) {
     return {"pointX": pointX, "pointY": pointY};
 }
 
-function flowerMotion(x, y) {
+function flowerMotion(x, y, deg) {
     const flower = document.querySelector(".flower-test");
-    flower.style.transform = `translate(${x}px, ${y}px)`;
+    flower.style.transform = `translate(${x}px, ${y}px) rotate(${deg}deg)`;
 }
 
+// Animates a petal falling on the screen
 // Based on: https://javascript.info/js-animation#structured-animation
 function animate(duration) {
     let start = performance.now();
 
     requestAnimationFrame(function animate(time) {
-        let timeLine = (time - start) / duration; // timeLine goes from 0 to 1
+        let timeLine = ((time - start) / duration); // timeLine goes from 0 to 1
         if (timeLine > 1) {
             timeLine = 1;
         } else if (timeLine < 0) {
             timeLine = 0;
         }
 
-        let {pointX, pointY} = bezierPath(cubicBezier(0, 0.35, 0.53, 1, timeLine));
+        let rotate = (cubicBezier(0, 0.84, 0.44, 1, timeLine)) * 360;
 
-        flowerMotion(pointX, pointY);
+        let frame = cubicBezier(0, 0.84, 0.44, 1, timeLine)// Find where the current frame is on the timeline
+        let {pointX, pointY} = cubicBezierPath(frame); 
+
+        flowerMotion(pointX, pointY, rotate); // Render frame
 
         (timeLine < 1) ? requestAnimationFrame(animate) : console.log("Animation done"); // Go to next frame
     });
