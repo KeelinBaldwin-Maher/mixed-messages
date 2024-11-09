6658703f4e2b231bac8a2afbda202f4fbb536a46
Keelin <keelin.baldwinmaher@gmail.com>
2024-11-09 16:17:11 -0700

    Animation refactoring

--- bkgAnimation.js
+++ bkgAnimation.js
@@ -1,12 +1,12 @@
 // Making a 4 point bezier curve. 
 // The math for this from: https://javascript.info/bezier-curve
 // P = ((1−t)^3 * a) + (3 * (1−t)^2 * t * b) + (3 * (1−t) * t^2 * c) + (t^3 * d)
-function cubicBezier(p1, p2, p3, p4, t) {
-    const point1 = Math.pow((1 - t), 3) * p1;
-    const point2 = 3 * Math.pow((1 - t), 2) * t * p2;
-    const point3 = 3 * (1 - t) * Math.pow(t, 2) * p3;
-    const point4 = Math.pow(t, 3) * p4;
-    return point1 + point2 + point3 + point4;
+function cubicBezier(p, t) {
+    const point1 = Math.pow((1 - t), 3) * p[0];
+    const point2 = 3 * Math.pow((1 - t), 2) * t * p[1];
+    const point3 = 3 * (1 - t) * Math.pow(t, 2) * p[2];
+    const point4 = Math.pow(t, 3) * p[3];
+    return (point1 + point2 + point3 + point4);
 }
 
 // 3 point curve
@@ -17,139 +17,175 @@ function quadBezier(p1, p2, p3, t) {
     return point1 + point2 + point3;
 }
 
-// Finds the coordinates of the object on a cubic bezier curve 
-function cubicBezierPath(xPoints, yPoints, f) {
-    const [x1, x2, x3, x4] = xPoints;
-    const [y1, y2, y3, y4] = yPoints;
+// Based on the frame, returns the coordinates of an object on a bezier path
+// The path is determined by the x and y points 
+function cubicBezierPath(xPoints, yPoints, frame) {
 
-    const pointX = cubicBezier(x1, x2, x3, x4, f);
-    const pointY = cubicBezier(y1, y2, y3, y4, f);
+    const pointX = cubicBezier(xPoints, frame);
+    const pointY = cubicBezier(yPoints, frame);
 
-    return { "pointX": pointX, "pointY": pointY };
+    return { "x": pointX, "y": pointY };
 }
 
-// Animates single petal falling on the screen, randomizes path and rotation
+function randomMax(max) {
+    return (Math.random() * max).toFixed(2);
+}
+
+function randomMinMax(min, max) {
+    return (Math.random() * (max - min) + min).toFixed(2);
+}
+
+function randomIntMax(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function randomIntMinMax(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+/**
+ * Creates a bezier path for objects to follow.
+ * @returns The x and y points of a bezier curve path
+ */
+function createPath() {
+    const width = window.innerWidth;
+    const widthFifth = width / 5;
+    const widthThird = (width / 3) + 20;
+    const widthHalf = width / 2;
+    const widthOver = width + 10;
+    const height = window.innerHeight;
+    const heightThird = height / 3;
+    const heightHalf = height / 2;
+
+    // Coordinates for bezier path, the values are window pixel coordinates
+    const xPoints = [
+        randomMinMax(-100, widthThird), // Start between 100px to the left and 1/3 of the window
+        randomMinMax(widthThird, widthHalf),
+        randomMax(width),
+        randomMinMax(widthFifth, widthOver) // Land between the last 2/3 of the window and 100px to the right
+    ];
+
+    const yPoints = [
+        randomMinMax(-45, heightThird),// Start between 45px above the window height and 1/3 of the window height
+        randomMax(heightHalf),
+        randomMinMax(heightHalf, height),
+        height
+    ];
+
+    return { xPoints: xPoints, yPoints: yPoints };
+}
+
+function createTimer(duration) {
+    const timer = {
+        duration: duration, // Duration in milliseconds
+        complete: false,
+    };
+    return timer;
+}
+
 // Based on: https://javascript.info/js-animation#structured-animation
-function animateFalling(imgId, duration) {
-    let start = performance.now();
-
-    // Path timing, a random 4 point bezier curve
-    const [f1, f2, f3, f4] = [0, 
-        (Math.random() * 0.5).toFixed(2), 
-        (Math.random() * (0.9 - 0.5) + 0.5).toFixed(2), 
-        1];
-
-    // Coordinates for bezier path, goes point 1 -> point 4
-    // values are for pixels    
-    const xPoints = [(Math.random() * (window.innerWidth * 0.4)).toFixed(2),
-    (Math.random() * window.innerWidth).toFixed(2),
-    (Math.random() * window.innerWidth).toFixed(2),
-    (Math.random() * (window.innerWidth - (window.innerWidth * 0.2)) + (window.innerWidth * 0.2)).toFixed(2)];
-    const yPoints = [(Math.random() * -45).toFixed(2), 
-    (Math.random() * (window.innerHeight * 0.5)).toFixed(2),
-    (Math.random() * (window.innerHeight - (window.innerHeight * 0.5)) + (window.innerHeight * 0.5)).toFixed(2), 
-    window.innerHeight];
-
-    // Rotation timing
-    const [r1, r2, r3, r4] = [0, Math.random().toFixed(2), Math.random().toFixed(2), 1];
-
-    requestAnimationFrame(function animate(time) {
-        let timeLine = ((time - start) / duration) > 1 ? 1 :
-            ((time - start) / duration) < 0 ? 0 : ((time - start) / duration);
-        // timeLine needs to go from 0 to 1
-
-        let timing = cubicBezier(f1, f2, f3, f4, timeLine); // frame timing
-        let { pointX, pointY } = cubicBezierPath(xPoints, yPoints, timing); // frame position
-
-        let rotate = (cubicBezier(r1, r2, r3, r4, timeLine)) * 360; // Next rotation degree
-
-        flowerMotion(imgId, pointX, pointY, rotate); // Render frame
-
-        (timeLine < 1) ? requestAnimationFrame(animate) : ""; // Go to next frame
+// Needs to have duration
+function animate(animationElement) {
+    let start;
+    
+    window.requestAnimationFrame((timeStamp) => {
+        start = timeStamp;
+        innerAnimate(timeStamp, animationElement, createTimer(randomIntMax(10000)));
     });
-}
 
-// Renders the current position and rotation of flower or petal
-function flowerMotion(imgId, x, y, r) {
-    const flower = document.querySelector(`#${imgId}`);
-    flower.style.transform = `translate(${x}px, ${y}px) rotate(${r}deg)`;
-}
+    function innerAnimate(timeStamp, aniSettings, timer) {
+        let frame
 
-// Creates an array of flower elements with lengths 4 - 10 and randomized images.
-// Places the images on the screen
-// Returns an array of flower ids
-function generateFlowers(groupNum) {
-    const flowers = new Array(Math.floor(Math.random() * (11 - 4) + 4)); // Array length between 4 and 10
-    const flowerSrc = [`./SVG/flowerDark.svg`, `./SVG/flowerMed.svg`, `./SVG/flowerLight.svg`];
+        if (timer.complete) {
+            frame = ((timeStamp - start) / aniSettings.duration); // frame values: 0 - 1
 
-    const flowerIds = new Array(flowers.length);
+            if (frame > 1 && aniSettings.loop) {
+                start = timeStamp;
+                frame = 0;
+                aniSettings.randomizeSettings();
+            } else if (frame > 1 && !aniSettings.loop) {
+                frame = 1;
+            }
 
-    for (let i = 0; i < flowers.length; i++) {
-        flowerIds[i] = `flower${i}${groupNum}`;
-        flowers[i] = `<img id="${flowerIds[i]}" class="flower"` +
-            `style="z-index: ${Math.floor(Math.random() * 10)}"` +
-            `src="${flowerSrc[Math.floor(Math.random() * flowerSrc.length)]}"></img>`;
-    }
+            aniSettings.render(frame);
 
-    for (const flower of flowers) {
-        document.querySelector(".cherry_tree").insertAdjacentHTML("afterbegin", flower);
-    }
-
-    return flowerIds;
-}
-
-// Creates an array of petal img elements with lengths 4 - 10 and randomized images.
-// Places the images on the screen
-// Returns array of petal ids
-function generatePetals(groupNum) {
-    const petals = new Array(Math.floor(Math.random() * (16 - 5) + 5)); // Array length between 4 and 10
-    const petalSrc = [`./SVG/petalDark.svg`, `./SVG/petalMed.svg`, `./SVG/petalLight.svg`];
-
-    const petalIds = new Array(petals.length);
-
-    for (let i = 0; i < petals.length; i++) {
-        petalIds[i] = `petal${i}${groupNum}`;
-        petals[i] = `<img id="${petalIds[i]}" class="petal"` +
-            `style="z-index: ${Math.floor(Math.random() * 10)}"` +
-            `src="${petalSrc[Math.floor(Math.random() * petalSrc.length)]}"></img>`;
-    }
-
-    for (const petal of petals) {
-        document.querySelector(".cherry_tree").insertAdjacentHTML("afterbegin", petal);
-    }
-
-    return petalIds;
-}
-
-// Fully animate petals and flowers
-function petalsAndFlowersAnimation() {
-    const petalIds = generatePetals(Math.floor(Math.random() * 1000)); // Array of petal img ids
-    for (const imgId of petalIds) {
-        const timeOut = Math.floor(Math.random() * (5000 - 2500) + 2500);
-        const duration = Math.floor(Math.random() * (5000 - 2500) + 2500);
-        setTimeout(animateFalling(imgId, duration), timeOut);
-    }
-
-    const flowerIds = generateFlowers(Math.floor(Math.random() * 1000));
-    for (const imgId of flowerIds) {
-        const timeOut = Math.floor(Math.random() * (5000 - 2500) + 2500);
-        const duration = Math.floor(Math.random() * (5000 - 2500) + 2500);
-        setTimeout(animateFalling(imgId, duration), timeOut);
-    }
-
-    setTimeout(() => {// Remove petals and flowers
-        for (const imgId of petalIds) {
-            document.querySelector(`#${imgId}`).remove();
+        } else {
+            frame = ((timeStamp - start) / timer.duration);
+            if (frame > 1) {
+                timer.complete = true;
+                start = timeStamp;
+                frame = 0;
+            }
         }
-        for (const imgId of flowerIds) {
-            document.querySelector(`#${imgId}`).remove();
+        
+        if (frame < 1) {
+            window.requestAnimationFrame((timeStamp) => {
+                innerAnimate(timeStamp, aniSettings, timer); // Go to next frame
+            });
         }
-    }, 6000);
+    }
 }
 
-function autoAnimatePetalsAndFlowers() {
-    petalsAndFlowersAnimation();
-    setInterval(petalsAndFlowersAnimation, 2000);
+// Creates a flower or petal element with randomized images.
+// Places the img on the screen
+// Returns image object
+function generateImg(type) {
+    const image = {
+        duration: randomIntMinMax(3000, 5000),
+        loop: true,
+        path: createPath(),
+        pathTiming: [0, randomMax(0.5), randomMinMax(0.4, 0.85), 1],
+        rotationTiming: [randomMinMax(-2, 2), randomMinMax(-2, 2), randomMinMax(-2, 2), randomMinMax(-2, 2)],
+        imgHTML: createImgHTML(type),
+        render(time) {
+            let rotate = cubicBezier(this.rotationTiming, time); // Just the x value of a bezier curve
+            let r = rotate * 360;
+
+            let pathPoint = cubicBezier(this.pathTiming, time);
+            let { x, y } = cubicBezierPath(this.path.xPoints, this.path.yPoints, pathPoint);
+
+            this.imgHTML.style.transform = `translate(${x}px, ${y}px) rotate(${r}deg)`;
+            if (time >= 1) this.imgHTML.remove();
+        },
+        randomizeSettings() {
+            this.path = createPath();
+            this.pathTiming = [0, randomMax(0.5), randomMinMax(0.4, 0.85), 1];
+            this.rotationTiming = [randomMinMax(-2, 2), randomMinMax(-2, 2), randomMinMax(-2, 2), randomMinMax(-2, 2)]
+        }
+    };
+
+    return image;
 }
 
-autoAnimatePetalsAndFlowers();
+function createImgHTML(type) {
+    const src = [`./SVG/${type}Dark.svg`, `./SVG/${type}Med.svg`, `./SVG/${type}Light.svg`];
+    imgHTML = new Image();
+
+    let height;
+    if (type === "flower") {
+        height = randomIntMinMax(3.5, 5) + "%";
+    } else if (type === "petal") {
+        height = randomIntMinMax(2, 2.65) + "%";
+    }
+
+    imgHTML.id = `${type}${randomIntMax(10)}`;
+    imgHTML.classList.add(type);
+    imgHTML.style["z-index"] = `${randomIntMax(10)}`;
+    imgHTML.style.height = height;
+    imgHTML.src = `${src[randomIntMax(src.length)]}`;
+    
+    document.querySelector(".cherry_tree").insertAdjacentElement("afterbegin", imgHTML);
+
+    return imgHTML;
+}
+
+
+for (let x = 0; x < 5; x++) {
+    const flower = generateImg("flower");
+    animate(flower);
+}
+
+for (let x = 0; x < 30; x++) {
+    const petal = generateImg("petal");
+    animate(petal);
+}
